926358227e90df9ab5e163b181eec1bc371ab49f
Codrut Ursache <ursache.codrut71@gmail.com>
2022-02-09 14:07:37 +0200

    add keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,8 @@
+-- ------------------------
+-- Keymaps configuration file
+-- ------------------------
+
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+
